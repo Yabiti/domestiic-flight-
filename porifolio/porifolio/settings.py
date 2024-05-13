1b288f62bd4a3fd15deab9bca6d925256479b957
Yabiti <yabetseyohannes@gmail.com>
2024-05-13 17:23:31 +0300

    .

--- porifolio/porifolio/settings.py
+++ porifolio/porifolio/settings.py
@@ -120,6 +120,9 @@ STATIC_URL = 'static/'
 MEDIA_ROOT = '/MEDIA/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATIC_DIRS = (
+    os.path.join(BASE_DIR, 'static')
+)
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
